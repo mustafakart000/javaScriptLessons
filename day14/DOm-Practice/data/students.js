3b8af96abd67558c2ab57ce0e9c251549aa5f011
Mustafa kart <mustafakart000@gmail.com>
2024-03-21 18:17:13 +0300

    changeRowColor

---
+++ day14/DOm-Practice/data/students.js
@@ -0,0 +1,7 @@
+export const students= [
+    {id: 1, name: "Ayse", point: 80},
+    {id: 2, name: "veli", point: 32},
+    {id: 3, name: "Fatma", point: 56},
+    {id: 4, name: "Ahmet", point: 79},
+    {id: 5, name: "Ayse", point: 44},   
+]
